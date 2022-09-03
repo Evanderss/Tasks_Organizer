f10d7a22268749d960d50eb7119a6da277e00bff
Evanderss <evanderramirez150@gmail.com>
2022-09-02 23:46:25 -0500

    Finalizando el creador de tareas, guardando los datos en la tabla de postgre y haciendo la url dinamica

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
+from .models import Task
 
 # Create your views here.
 def list_task(request):
     return render(request ,"list_task.html")
 
 def create_task(request):
-    print(request.POST)
+    task = Task(title=request.POST["title"], description=request.POST["description"], time_to_do=request.POST["time_to_do"])
+    task.save()
     return redirect("/tasks/")
